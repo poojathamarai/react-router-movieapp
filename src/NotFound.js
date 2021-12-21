f808af513958658b725bcc8c9c0322bae1e19ff2
Pooja <92292344+poojathamarai@users.noreply.github.com>
2021-12-21 09:57:54 +0530

    Update NotFound.js

--- src/NotFound.js
+++ src/NotFound.js
@@ -2,7 +2,7 @@ export function NotFound() {
   return (
     <div className="not-found-container">
       <h2>404</h2>
-      <img className="not-found-image" src="https://assets.materialup.com/uploads/26541cce-49c6-4e35-a055-e11b90ffad68/preview.gif"  />
+      <img className="not-found-image" src="https://assets.materialup.com/uploads/26541cce-49c6-4e35-a055-e11b90ffad68/preview.gif" alt=" " />
     </div>
   );
 }
